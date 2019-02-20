f4ebb17fe78e114a64f0d7d62a98fc46d8807091
Darya Shogol <da18121996@yandex.ru>
2019-02-20 11:38:36 +0300

    Image creation and recording

---
+++ BookShop/Managers/ImageManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.Managers.Interfaces;
+
+namespace BookShop.Managers
+{
+    public class ImageDescription
+    {
+        public byte[] Bytes { get; set; }
+        public string Extension { get; set; }
+    }
+    public class ImageManager : IImageManager
+    {
+        public ImageDescription ConvertImage(string base64)
+        {
+            var parts = base64.Split(";base64,");
+            if (parts.Length < 2)
+            {
+                throw new Exception("Base64 string is expected");
+            }
+            ImageDescription result = new ImageDescription();
+            var base64String = parts[1];
+            result.Extension = "." + parts[0].Split("data:image/")[1];
+            if (base64String == null || base64String.Length == 0 || base64String.Length % 4 != 0
+                || base64String.Contains(" ") || base64String.Contains("\t") || base64String.Contains("\r") || base64String.Contains("\n"))
+            {
+                throw new Exception("Base64 string is expected");
+            }
+            result.Bytes = Convert.FromBase64String(base64String);
+
+            return result;
+        }
+    }
+}
---
+++ BookShop/Managers/ImageStorageManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.Dtos.Misc;
+using BookShop.Managers.Interfaces;
+using BookShop.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BookShop.Managers
+{
+    public class ImageStorageManager : IImageStorageManager
+    {
+        const string TempFolder = "TempPhotos";
+        const string Folder = "Photos";
+        string tempFolderPath = "";
+        string folderPath = "";
+        string root = "";
+        IImageManager manager;
+
+        public ImageStorageManager(IHostingEnvironment env, IImageManager m)
+        {
+            manager = m;
+            root = env.WebRootPath;
+            tempFolderPath = Path.Combine(root, TempFolder);
+            folderPath = Path.Combine(root, Folder);
+            CreateFoldersIfNeed();
+        }
+
+        private void CreateFoldersIfNeed()
+        {
+            if (!Directory.Exists(tempFolderPath))
+            {
+                Directory.CreateDirectory(tempFolderPath);
+            }
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+        }
+
+        private string GetFileName()
+        {
+            return Path.GetRandomFileName().Split(".")[0];
+        }
+
+        public ImageDto CreateTempFiles(ImageDto dto)
+        {
+            var name = GetFileName();
+            return new ImageDto()
+            {
+                Original = CreateTempFile(dto.Original, name),
+                Title = dto.Title
+               // Cropped = CreateTempFile(dto.Cropped, name, "_cropped")
+            };
+        }
+
+        private string CreateTempFile(string base64, string name, string suffix = "")
+        {
+            var image = manager.ConvertImage(base64);
+            var tempName = Path.Combine(tempFolderPath, name + suffix + image.Extension);
+            File.WriteAllBytes(tempName, image.Bytes);
+            return Path.Combine(TempFolder, name + suffix + image.Extension);
+        }
+
+        public ImageDto CreateTempFile(string base64)
+        {
+            var name = GetFileName();
+            return new ImageDto() { Original = CreateTempFile(base64, name) };
+        }
+
+        public ImageDto MoveToPersistentLocation(ImageDto dto)
+        {
+            return new ImageDto()
+            {
+                Original = MoveToPersistentLocation(dto.Original),
+                Title = dto.Title
+               // Cropped = MoveToPersistentLocation(dto.Cropped)
+            };
+        }
+
+        public string MoveToPersistentLocation(string name)
+        {
+            var fileName = name.Split(TempFolder)[1].Split("\\")[1];
+            var persistent = Path.Combine(folderPath, fileName);
+            var path = Path.Combine(root, name);
+            File.Copy(path, persistent);
+            File.Delete(path);
+            return Path.Combine(Folder, fileName);
+        }
+
+        public Image OverwritePersistentLocation(Image src, Image dst)
+        {
+            return new Image()
+            {
+                Original = OverwritePersistentLocation(src.Original, dst.Original),
+                Title = src.Title
+               // Cropped = OverwritePersistentLocation(src.Cropped, dst.Cropped)
+            };
+        }
+
+        private string OverwritePersistentLocation(string name, string dts)
+        {
+            var path = Path.Combine(root, name);
+            var persPath = Path.Combine(root, dts);
+            File.Copy(path, persPath, true);
+            File.Delete(path);
+            return dts;
+
+        }
+    }
+}
