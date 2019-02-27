2e10dedb187731bb4efee5d0347c4812f2766a30
Darya Shogol <da18121996@yandex.ru>
2019-02-27 14:58:47 +0300

    Add menu

---
+++ BookShop/wwwroot/app/Layout/Header.vue
@@ -0,0 +1,28 @@
+﻿<template>
+	<div class="header">
+		<main-menu></main-menu>
+	</div>
+</template>
+
+<script>
+	import MainMenu from './Menu.vue';
+
+	export default {
+		components:{
+			MainMenu
+		}
+	};
+</script>
+
+<style>
+	.header {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		height: 50px;
+		border: solid 1px gray;
+		justify-content: space-between;
+	}
+</style>
+
+
--- BookShop/wwwroot/app/main.js
+++ BookShop/wwwroot/app/main.js
@@ -1,24 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+//import Layout from './components/Layout/Header.vue';
+import Layout from './Layout/Header.vue';
+import VueCroppie from 'vue-croppie';
 
-Vue.config.productionTip = false
-Vue.use(VueRouter)
+import BootstrapVue from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+//import './styles/main.css';
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+Vue.use(VueCroppie);
 
 const routes = [
     {
         path: '/',
         component: App
     }
-]
+];
 
 const router = new VueRouter({
     routes,
     mode: 'history'
-})
+});
 
 new Vue({
     el: '#app',
-    template: "<div><router-view></router-view></div>",
+    template: "<div><layout></layout></div>",
+    components: {
+        'layout': Layout
+    },
     router
 })
